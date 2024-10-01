1227b3c5301a0f066b8f233fd2a2f6881ee4a820
Sam Alba <216487+samalba@users.noreply.github.com>
2024-10-01 16:30:10 -0700

    initial commit
    Signed-off-by: Sam Alba <216487+samalba@users.noreply.github.com>

---
+++ app.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"embed"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+//go:embed templates/*
+var resources embed.FS
+
+var t = template.Must(template.ParseFS(resources, "templates/*"))
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := map[string]string{
+			"text": os.Getenv("TEXT"),
+		}
+
+		t.ExecuteTemplate(w, "index.html.tmpl", data)
+	})
+
+	log.Println("listening on", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
